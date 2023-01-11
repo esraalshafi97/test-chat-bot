2bb59223e7787dcf7aa6cd40ff559fb785443743
esra alshafi <esra.alshafi@gmail.com>
2023-01-11 14:24:05 +0200

    first commit

---
+++ src/controller/homepageController.js
@@ -0,0 +1,66 @@
+require ("dotenv").config();
+
+const MY_VERIFY_TOKEN=process.env.MY_TOKEN;
+
+
+let getHomePage=(req,res)=>{
+    return res.render("homepage.ejs")
+};
+
+let getWebhook=(req,res)=>{
+    console.log("MY_VERIFY_TOKEN");
+    console.log(MY_VERIFY_TOKEN);
+
+    const VERIFY_TOKEN=MY_VERIFY_TOKEN;
+// Parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+
+
+
+}
+let postWebhook=(req,res)=>{
+    let body = req.body;
+
+    console.log(`\u{1F7EA} Received webhook:`);
+    console.dir(body, { depth: null });
+
+    if (body.object === "page") {
+        body.entry.array.forEach(entry => {
+                    let webhook_event=entry.messaging[0];
+                    console.log(webhook_event);
+                });
+
+
+        // Returns a '200 OK' response to all requests
+        res.status(200).send("EVENT_RECEIVED");
+   // ...
+        // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
+   // ...
+      } else {
+        // Return a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+      }
+
+
+}
+module.exports={
+    getHomePage:getHomePage,
+    getWebhook:getWebhook,
+    postWebhook:postWebhook
+
+};
---
+++ src/routes/web.js
@@ -0,0 +1,13 @@
+import express from "express";
+import homepageController from "../controller/homepageController.js";
+
+let router=express.Router();
+
+let initWebRoutes=(app)=>{
+    router.get("/", homepageController.getHomePage);
+    router.get("/webhook",homepageController.getWebhook);
+    router.post("/webhook",homepageController.postWebhook);
+    return app.use("/",router);
+};
+
+module.exports=initWebRoutes;
